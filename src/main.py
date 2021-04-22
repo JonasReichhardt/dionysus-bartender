db1cc12c6ad8ebdfd04d42af012e77e3856c4cb5
Jonas Reichhardt <JonasReichhardt>
2021-04-22 21:04:28 +0200

    add picture

--- src/main.py
+++ src/main.py
@@ -6,6 +6,9 @@ from kivy.uix.carousel import Carousel
 from kivy.uix.button import Button
 from kivy.uix.behaviors import ButtonBehavior
 from kivy.uix.carousel import Carousel
+from kivy.uix.image import Image
+from kivy.uix.anchorlayout import AnchorLayout
+
 
 import time
 
@@ -58,18 +61,13 @@ class GPButton(Button):
         else:
             print(f'\033[93m[INF] gpio {self.pin1} | low\033[0m')
 
-
-class CocktailPage(GridLayout):
-    def __init__(self, **kwargs):
-        super(GridLayout, self).__init__(**kwargs)
-
 class Program(App):
 
     def build(self):
         root = Carousel()
         page1 = GridLayout(cols=2, rows = 3, padding = 10, spacing=10)
 
-        btn1 = GPButton(24,11,text='Pump 2')
+        btn1 = GPButton(24,11,text='Pump 2',size=(75,50),size_hint=(.5,.25))
         btn2 = GPButton(18,23,text='Pump 1')
         btn3 = GPButton(19,26,text='Pump 4')
         btn4 = GPButton(6,13,text='Pump 3')
@@ -83,13 +81,29 @@ class Program(App):
         page1.add_widget(btn6)
         page1.add_widget(btn5)
 
-        root.add_widget(page1)
-        root.add_widget(CocktailPage())
+        page2 = AnchorLayout()
+        img = Image(source='res\\img\\coke.png')
+        page2.add_widget(img)
 
+        page3 = AnchorLayout()
+        img = Image(source='res\\img\\martini.png')
+        page3.add_widget(img)
+
+        page4 = AnchorLayout()
+        img = Image(source='res\\img\\dik.png')
+        page4.add_widget(img)
+        
+        # add pages to carousel
+        root.add_widget(page1)
+        root.add_widget(page2)
+        root.add_widget(page3)
+        root.add_widget(page4)
+        
         return root
 
 
 if __name__ == '__main__':
+    Window.clearcolor = (1,1,1,1)
     if fullscreen==1:
         Window.fullscreen = 'auto'
     Program().run()
