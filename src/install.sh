8b8a8da847800d47e4df2857a6134cd5778f79d1
jonasreichhardt <reichhardt.jonas@gmail.com>
2021-07-15 17:57:58 +0200

    add install and start script

---
+++ src/install.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+bold="\e[1m"
+uline="\e[4m"
+reset="\e[0m"
+green="\e[0;92m"
+blue="\e[0;94m"
+
+echo -e "${blue}${bold}${uline}[Dionysus-Bartender]${reset}"
+echo " "
+
+echo -e "${blue}${bold}${uline}[Phase 1 of 5]${reset}"
+echo -e "${green}${bold}[Installing system dependencies]${reset}"
+echo -e "${green}Updating package list...${reset}"
+sudo apt update
+echo " "
+
+echo -e "${green}Installing window provider...${reset}"
+sudo apt install pkg-config libgl1-mesa-dev libgles2-mesa-dev \
+   libgstreamer1.0-dev \
+   gstreamer1.0-plugins-{bad,base,good,ugly} \
+   gstreamer1.0-{omx,alsa} libmtdev-dev \
+   xclip xsel libjpeg-dev
+sudo apt install libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev
+echo " "
+
+echo -e "${blue}${bold}${uline}[Phase 2 of 5]${reset}"
+echo -e "${green}${bold}[Setting up virtual environment]${reset}"
+echo -e "${green}Installing virtual environment...${reset}"
+cd kivymd
+python3 -m venv virt
+echo -e "${green}Starting virtual environment...${reset}"
+source virt/bin/activate
+echo " "
+
+echo -e "${blue}${bold}${uline}[Phase 3 of 5]${reset}"
+echo -e "${green}${bold}[Installing KivyMD dependencies]${reset}"
+pip install Pillow
+pip install kivy
+echo " "
+
+echo -e "${blue}${bold}${uline}[Phase 4 of 5]${reset}"
+echo -e "${green}${bold}[Installing KivyMD]${reset}"
+echo -e "${green}Cloning repository...${reset}"
+git clone https://github.com/kivymd/KivyMD.git --depth 1
+echo " "
+echo -e "${green}Building framework...${reset}"
+cd KivyMD
+pip install .
+echo " "
+
+echo -e "${blue}${bold}${uline}[Phase 5 of 5]${reset}"
+echo -e "${green}${bold}[Copying config]${reset}"
+sudo cp ../../config/kivy/config.ini ~/.kivy/config.ini 
+sudo cp ~/.kivy/config.ini /root/.kivy/config.ini
+
+read -n 1 -s -r -p "Press any key to continue"
