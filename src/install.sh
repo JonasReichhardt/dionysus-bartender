79449a6f7447d4cfcb680ed7bb5222b8578ce9a8
Jonas Reichhardt <32189954+JonasReichhardt@users.noreply.github.com>
2021-07-16 12:28:17 +0200

    Update install.sh

--- src/install.sh
+++ src/install.sh
@@ -20,12 +20,12 @@ sudo apt install pkg-config libgl1-mesa-dev libgles2-mesa-dev \
    libgstreamer1.0-dev \
    gstreamer1.0-plugins-{bad,base,good,ugly} \
    gstreamer1.0-{omx,alsa} libmtdev-dev \
-   xclip xsel libjpeg-dev
-sudo apt install libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev
+   xclip xsel libjpeg-dev -y
+sudo apt install libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev -y
 echo " "
 
 echo -e "${green}Installing unclutter...${reset}"
-sudo apt-get install unclutter
+sudo apt-get install unclutter -y
 echo " "
 
 echo -e "${blue}${bold}${uline}[Phase 2 of 6]${reset}"
