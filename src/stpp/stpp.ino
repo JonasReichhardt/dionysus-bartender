e6fbca5d8e03665365d4f9d0d76c1222e7994696
JonasReichhardt <reichhardt.jonas@gmail.com>
2022-03-13 17:39:33 +0100

    finish stpp impl

--- src/stpp/stpp.ino
+++ src/stpp/stpp.ino
@@ -1,14 +1,17 @@
-#define pin1 8
-#define pin2 9
-#define pin3 10
-#define pin4 11
-#define pin5 12
-#define pin6 13
+#define pin1 22
+#define pin2 23
+#define pin3 24
+#define pin4 25
+#define pin5 26
+#define pin6 27
 
-#define DEBUG false
+// preprocessor options
+#define DEBUG_UPDATE_PINS false
+#define DEBUG_CHECK_INPUT false
+#define PRINT_READY_MSG true
+#define DEBUG_RESET false
 
-unsigned long starts[6];
-long waitTimes[6];
+unsigned long endTimes[6];
 bool setFlags[] = {false,false,false,false,false,false};
 int pins[] = {pin1,pin2,pin3,pin4,pin5,pin6};
 
@@ -20,6 +23,9 @@ void setup() {
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB
   }
+  #if PRINT_READY_MSG
+    Serial.println("RDY");
+  #endif
 }
 
 void loop() {
@@ -30,11 +36,10 @@ void loop() {
 void update_pins(){
   for (int i = 0; i < 6; i = i + 1) {
     if(setFlags[i]){
-      if(millis() - starts[i] >= waitTimes[i] ){
-        #if DEBUG
-          Serial.print(pins[i],DEC);
-          Serial.print(":");
-          Serial.println(millis() - starts[i]);
+      if(millis() >= endTimes[i]){
+        #if DEBUG_UPDATE_PINS
+          Serial.print(i,DEC);
+          Serial.println(":LOW");
     	  #endif
         setFlags[i] = false;
         digitalWrite(pins[i],LOW);
@@ -44,21 +49,41 @@ void update_pins(){
 }
 
 void check_for_input(){
-  if (Serial.available() > 0) {
-    long data = Serial.parseInt(SKIP_NONE);
-
-    // last digit = index of pump array   
-    int index = data % 10;
-    data = data / 10;
-
-    setFlags[index] = true;
-    waitTimes[index] = data;
-    starts[index] = millis();
-    digitalWrite(pins[index],HIGH);
-    #if DEBUG
-        Serial.print(pins[index],DEC);
-        Serial.print(":");
-        Serial.println(data,DEC);
-    #endif
+  // cancel cocktail
+  if(Serial.peek()=='x'){
+    reset();
+    Serial.read();
+    return;
   }
+
+  // check for new input
+  if (Serial.available() > 10) {
+    for(int i=0; i<6; i=i+1){
+      long data = Serial.parseInt();
+      
+      if(data > 0 && !setFlags[i]){
+        setFlags[i] = true;
+        endTimes[i] = millis()+data;
+        digitalWrite(pins[i],HIGH);
+        
+        #if DEBUG_CHECK_INPUT
+            Serial.print(i,DEC);
+            Serial.print(":");
+            Serial.println(data,DEC);
+        #endif
+      }
+    }
+    // remove last byte from buffer
+    Serial.read();
+  }
+}
+
+void reset(){
+  for(int i=0; i<6; i=i+1){
+    digitalWrite(pins[i],LOW);
+    setFlags[i] = false;
+  }
+  #if DEBUG_RESET
+    Serial.println("RESET");
+  #endif
 }
