0a17f7ae6628f4c918fe15eb28f9cb6e9d999607
Jonas Reichhardt <j.reichhardt@tig.at>
2022-05-13 14:36:23 +0200

    add data interface

--- src/webserver/server.js
+++ src/webserver/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const fs = require("fs")
 const { SerialPort } = require('serialport')
+var serialEnabled = true
 
 main()
 
@@ -16,7 +17,7 @@ async function main() {
     app.post('/cocktails', (req, res) => {
         // check if cocktails exists
         index = req.body.cocktail - 1
-        if (index < 0 != index > cocktails.length) {
+        if (index < 0 || index > cocktails.length) {
             res.status(404).send('no cocktail with given id')
             return
         }
@@ -26,23 +27,32 @@ async function main() {
         res.status(200).send('preparing cocktail')
     })
 
+    app.get('/cocktails', (req, res) => {
+        res.status(200).send(cocktails)
+    })
+
+    app.get('/pumps', (req, res) => {
+        res.status(200).send(pumps)
+    })
+
     var server = app.listen(settings.port, function () {
         var host = require('ip').address()
         var port = server.address().port
-        console.log("Server listening at http://%s:%s", host, port)
+        console.info("INF| Server listening at http://%s:%s", host, port)
     })
 }
 
 async function open_serial_port() {
     let portList = await SerialPort.list()
-    if (portList != undefined) {
+    if (portList != undefined && portList.length > 0) {
         return new SerialPort({ path: portList[0].path, baudRate: 115200 }, function (err) {
             if (err) {
-                return console.error('could not open port |', err.message)
+                return console.error('ERR| could not open port |', err.message)
             }
         })
     } else {
-        console.error("no serial port found")
+        console.error("ERR| no serial port found")
+        serialEnabled = false
     }
 }
 
@@ -50,6 +60,7 @@ function load_app_settings() {
     try {
         return JSON.parse(fs.readFileSync('appsettings.json', 'utf8'))
     } catch (err) {
+        console.error("ERR| could not load app settings")
         console.error(err)
     }
 }
@@ -58,6 +69,7 @@ function load_cocktails() {
     try {
         return JSON.parse(fs.readFileSync('..\\res\\cocktails.json', 'utf8')).cocktails
     } catch (err) {
+        console.error("ERR| could not load cocktail config")
         console.error(err)
     }
 }
@@ -66,20 +78,21 @@ function load_ingredients() {
     try {
         return JSON.parse(fs.readFileSync('..\\res\\pump-config.json', 'utf8')).pumps
     } catch (err) {
+        console.error("ERR| could not load pump config")
         console.error(err)
     }
 }
 
 function makeCocktail(index, cocktails, pumps, serial) {
     let cocktail = cocktails[index]
-    let command = [0,0,0,0,0,0,]
+    let command = [0, 0, 0, 0, 0, 0,]
 
     for (let i = 0; i < cocktail.ingredients.length; i++) {
         for (let j = 0; j < pumps.length; j++) {
-            if(pumps[j].ingredient == cocktail.ingredients[i].name){
+            if (pumps[j].ingredient == cocktail.ingredients[i].name) {
                 let pumpTime = cocktail.ingredients[i].amount / pumps[j].flowrate
                 // convert to milliseconds
-                command[j] = pumpTime*1000
+                command[j] = pumpTime * 1000
             }
         }
     }
@@ -87,18 +100,22 @@ function makeCocktail(index, cocktails, pumps, serial) {
     sendCommand(command, serial)
 }
 
-function sendCommand(command, serial){
-    if(command.length>6){
-        console.error("command too large")
+function sendCommand(command, serial) {
+    if (command.length > 6) {
+        console.error("ERR| command too large")
         return
     }
     msg = command.join('|').concat('|')
-    console.log("writing command <%s>",msg)
-    serial.write(msg,
-        function (err) {
-            if (err) {
-                return console.error('could not write to port |', err.message)
+    console.info("INF| writing command <%s>", msg)
+
+    // only send if serial is enabled
+    if(serialEnabled){
+        serial.write(msg,
+            function (err) {
+                if (err) {
+                    return console.error('ERR| could not write to port |', err.message)
+                }
             }
-        }
-    )
+        )
+    }
 }
