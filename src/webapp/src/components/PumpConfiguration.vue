3038477508ef22daabe2f6300e242711d5db2226
shaslhofer <Ch.li.lu.se>
2022-05-24 16:53:31 +0200

    PumpConfiguration.vue initial commit

---
+++ src/webapp/src/components/PumpConfiguration.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Hello World!</h1>
+</template>
+
+<script>
+export default {
+  name: "PumpConfiguration",
+  data() {
+    return {
+      pumpConfig: ['osaft','osaft','osaft','osaft','osaft','osaft']
+    }
+  }
+}
+</script>
+
