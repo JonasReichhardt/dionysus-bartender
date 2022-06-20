733f2dfb8284bb32f9bb0c2df6dfcbc621dec167
Stefan Haslhofer <haslhofer.stefan@gmail.com>
2022-06-20 19:39:39 +0200

    Feat/sensor config (#21)
    * created webapp folder
    
    * pumps clean out pipes
    
    * added scale configuration
    
    * added delay to loop
    
    * updated stpp.ino added comments code cleanup
    
    * disabled debug pins
    
    * removed unnecessary readme.md
    
    * use NUM_OF_PUMPS constant for loops
    
    Co-authored-by: shaslhofer <Ch.li.lu.se>
    Co-authored-by: shaslhofer <haslhofer@unisoftwareplus.com>

--- src/stpp/stpp.ino
+++ src/stpp/stpp.ino
@@ -1,51 +1,91 @@
-#define pin1 22
-#define pin2 23
-#define pin3 24
-#define pin4 25
-#define pin5 26
-#define pin6 27
+#include <HX711_ADC.h>
+
+#define pin1 49
+#define pin2 53
+#define pin3 44
+#define pin4 42
+#define pin5 48
+#define pin6 52
+
+#define pin1r 47
+#define pin2r 51
+#define pin3r 45
+#define pin4r 43
+#define pin5r 46
+#define pin6r 50
 
 // preprocessor options
 #define DEBUG_UPDATE_PINS false
 #define DEBUG_CHECK_INPUT false
-#define PRINT_READY_MSG true
+#define PRINT_READY_MSG false
 #define DEBUG_RESET false
+#define DEBUG_WEIGHT false
 
-unsigned long endTimes[6];
+#define PUMP_DELAY 3000
+#define NUM_OF_PUMPS 6
+
+#define CAL_FACTOR -1058.28
+#define WEIGHT_THRESHOLD 100.0
+
+HX711_ADC LoadCell(22, 23);
+unsigned long endTimes[NUM_OF_PUMPS];
 bool setFlags[] = {false,false,false,false,false,false};
 int pins[] = {pin1,pin2,pin3,pin4,pin5,pin6};
+int rPins[] = {pin1r,pin2r,pin3r,pin4r,pin5r,pin6r};
 
 void setup() {
   Serial.begin(115200);
-  for (int i = 0; i < 6; i = i + 1) {
+  LoadCell.begin();
+  LoadCell.setCalFactor(CAL_FACTOR);
+  LoadCell.start(2000);
+  for (int i = 0; i < NUM_OF_PUMPS; i = i + 1) {
     pinMode(pins[i], OUTPUT);
+    pinMode(rPins[i], OUTPUT);
   }
+  
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB
   }
+  
   #if PRINT_READY_MSG
     Serial.println("RDY");
   #endif
 }
 
 void loop() {
+  delay(10); // delay needed for weight sensor
+  LoadCell.update();
   update_pins();
   check_for_input();
 }
 
 void update_pins(){
-  for (int i = 0; i < 6; i = i + 1) {
+  for (int i = 0; i < NUM_OF_PUMPS; i = i + 1) {
     if(setFlags[i]){
-      if(millis() >= endTimes[i]){
+      if(millis() >= endTimes[i] || !weightThresholdExceeded()){
+        setFlags[i] = false;
+        digitalWrite(pins[i],LOW);
+        
         #if DEBUG_UPDATE_PINS
           Serial.print(i,DEC);
           Serial.println(":LOW");
-    	  #endif
-        setFlags[i] = false;
-        digitalWrite(pins[i],LOW);
+        #endif
       }
     }
   }
+}
+
+bool weightThresholdExceeded() {
+  // load current weight on scale
+  float i = LoadCell.getData();
+  
+  #if DEBUG_WEIGHT
+    Serial.print("Load_cell output val: ");
+    Serial.println(i);
+  #endif
+  
+  // return true if measured weight exceeds threshold
+  return i > WEIGHT_THRESHOLD;
 }
 
 void check_for_input(){
@@ -54,35 +94,71 @@ void check_for_input(){
     reset();
     Serial.read();
     return;
+  } else if(Serial.peek() == 'r') {
+    emptyTubes();
+    Serial.read();
+    return;
   }
-
+  
   // check for new input
   if (Serial.available() > 10) {
-    for(int i=0; i<6; i=i+1){
-      long data = Serial.parseInt();
-      
-      if(data > 0 && !setFlags[i]){
-        setFlags[i] = true;
-        endTimes[i] = millis()+data;
-        digitalWrite(pins[i],HIGH);
+    // only start pumps if weight is on sensor (i.e. glas is beneath hose pipes)
+    if (weightThresholdExceeded()) {
+      for(int i=0; i < NUM_OF_PUMPS; i=i+1){
+        long data = Serial.parseInt();
         
-        #if DEBUG_CHECK_INPUT
-            Serial.print(i,DEC);
-            Serial.print(":");
-            Serial.println(data,DEC);
-        #endif
+        if(data > 0 && !setFlags[i]){
+          setFlags[i] = true;
+          endTimes[i] = millis()+data;
+          digitalWrite(pins[i],HIGH);
+          
+          #if DEBUG_CHECK_INPUT
+              Serial.print(i,DEC);
+              Serial.print(":");
+              Serial.println(data,DEC);
+          #endif
+        }
       }
+      // remove last byte from buffer
+      Serial.read();
+    } else {
+      serialFlush();
+      
+      #if DEBUG_WEIGHT
+        Serial.println("Serial data flushed");
+      #endif
     }
-    // remove last byte from buffer
-    Serial.read();
+  }
+}
+
+void serialFlush(){
+  // empty serial data
+  while(Serial.available() > 0) {
+    char t = Serial.read();
+  }
+}
+
+void emptyTubes() {
+  // start clearing the tubes
+  for (int i = 0; i < NUM_OF_PUMPS; i++) {
+    digitalWrite(rPins[i], HIGH);
+  }
+
+  // wait for 3 seconds
+  delay(PUMP_DELAY);
+
+  // stop rotating the pumps
+  for (int i = 0; i < NUM_OF_PUMPS; i++) {
+    digitalWrite(rPins[i], LOW);
   }
 }
 
 void reset(){
-  for(int i=0; i<6; i=i+1){
+  for(int i = 0; i < NUM_OF_PUMPS; i++){
     digitalWrite(pins[i],LOW);
     setFlags[i] = false;
   }
+  
   #if DEBUG_RESET
     Serial.println("RESET");
   #endif
